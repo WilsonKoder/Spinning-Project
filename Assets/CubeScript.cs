92bdfe0fae6f6ba41e7f880aafcdfad18c710ab6
WilsonKoder111 <WilsonKoder@Wilsons-MacBook-Pro.local>
2014-06-23 18:32:37 +0800

    Spinning Cube in Unity Initial Upload
    Initial upload

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	public float rotationSpeed = 25f;
+
+	// Use this for initialization
+	void Start () {
+		rotationSpeed = 25f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+	}
+}
